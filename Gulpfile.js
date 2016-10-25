38358526e4312ed5f739393d6dcde7052a3f6c68
Ben Oakenfull <benoakenfull@gmail.com>
2016-10-25 22:36:50 +0100

    Gulp setup and initial files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,20 @@
-var gulp = require("gulp");
-var mocha = require("gulp-mocha");
+var gulp = require("gulp"),
+    sass = require("gulp-sass")
+    mocha = require("gulp-mocha");
 
-gulp.task("default", function(){
-    gulp.src('test/test_sass.js', {read: false})
-      .pipe(mocha({reporter: 'nyan'}));
+gulp.task("default", ["test", "sass"]);
+
+gulp.task("test", function(){
+  gulp.src("test/test_sass.js", {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task("sass", function(){
+  return gulp.src('./src/wazooie.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task("watch", function(){
+  gulp.watch("./src/**/*.scss", ["sass", "test"]);
 });
